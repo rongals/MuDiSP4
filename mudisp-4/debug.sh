40edc749166363645a1cde7e2ff407698b5f0d4c
innocentimassimo <innocentimassimo.maxx@gmail.com>
2012-04-17 17:51:11 +0200

    Insert tool directory for param_exploder and launcher, update INSTALL doc

--- mudisp-4/debug.sh
+++ mudisp-4/debug.sh
@@ -3,6 +3,6 @@
 rm -rf _build_debug
 mkdir  _build_debug
 cd _build_debug	
-cmake -DCMAKE_INSTALL_PREFIX=/usr/local/mudisp4 -DCMAKE_BUILD_TYPE=Debug  ../
+cmake -DCMAKE_INSTALL_PREFIX=/usr/local/mudisp4 -DCMAKE_BUILD_TYPE=Debug -DENABLE_DOXYGEN=OFF  -DENABLE_UTILS=OFF ../
 make
 

86ed94da1a4a0b6ab3be33f882b6c19c1faa01ae
Luca Simone Ronga <luca.simone.ronga@gmail.com>
2012-05-31 17:43:41 +0200

    mcpcoeffs updated

--- mudisp-4-examples/gmc-cdma/src/mcpmpcoeffs.cpp
+++ mudisp-4-examples/gmc-cdma/src/mcpmpcoeffs.cpp
@@ -12,6 +12,27 @@
 #include "gsl/gsl_matrix.h"
 #include "gmccdma.h"
 #include "gsl/gsl_randist.h"
+#include "gsl/gsl_sf_trig.h"
+#include "gsl/gsl_math.h"
+
+//
+//
+// Matrix display
+//
+//
+void gsl_matrix_show(gsl_matrix *mat) {
+  unsigned int N=mat->size1, M=mat->size2;
+  for (int i=0; i<N; i++) {
+    for (int j=0; j<M; j++) {
+      double el = gsl_matrix_get(mat,i,j);
+      cout << setw(15)
+	   << el;
+    }
+    cout << endl;
+  }
+  cout << endl;
+}
+
 
 extern void gsl_matrix_complex_show(gsl_matrix_complex *);
 
@@ -54,6 +75,11 @@ void MCPMPCoeffs::Setup() {
   _M = M();
   ch = gsl_matrix_complex_alloc(_M*_M,N());
   ran = gsl_rng_alloc( gsl_rng_default );
+  runCount = 0;
+
+  // path loss matrix
+  pathLoss = gsl_matrix_alloc(_M,_M);
+  gsl_matrix_set_all(pathLoss,1.0);
 
   double pwr=0.0;
   for (int j=0; j<L(); j++) {
@@ -64,9 +90,9 @@ void MCPMPCoeffs::Setup() {
   gainrice=sqrt(Ricean()/(Ricean()+1.0));
 
 
-  cout << "Sum(taps^2)=" << pwr << endl
-       << "gain=" << gain << endl
-       << "gainrice=" << gainrice << endl;
+  // cout << "Sum(taps^2)=" << pwr << endl
+  //      << "gain=" << gain << endl
+  //      << "gainrice=" << gainrice << endl;
 
   //
   // GEO
@@ -75,9 +101,10 @@ void MCPMPCoeffs::Setup() {
   if (GeoFn()=="none") // check if GEO desired
     geoEnabled = 0;
   else { // GEO rendering enabled
-       geoEnabled = 1;
+    geoEnabled = 1;
     GeoInit();
     GeoRender();
+    PathLossUpdate();
   }
   
   
@@ -94,23 +121,17 @@ void MCPMPCoeffs::Run() {
   gsl_complex o = gsl_complex_rect(1,0);
 
   //
+  // Update Positions 
   //
-  // produce the idiot channel matrix
-  //
-  //
-  //   for (int i=0; i<USERS; i++) {
-  //     for (int j=0; j<N(); j++) {
-  //       if (j==0)
-  // 	gsl_matrix_complex_set(ch,i,j,o);
-  //       else if (j==1)
-  // 	gsl_matrix_complex_set(ch,i,j,gsl_complex_polar(0.0,M_PI/3.0));
-  //       else
-  // 	gsl_matrix_complex_set(ch,i,j,z);
+  if (geoEnabled && (runCount++ == GEO_UPDATE_INTERVAL)) {
+    GeoUpdate(5.0);
+    GeoRender();
+    PathLossUpdate();
+    runCount = 0;
+    cout << "UPDATING!" << endl;
+    // gsl_matrix_show(pathLoss);
 
-  //     }
-  //   }
-
-
+  }
 
   //
   // Exponentially decaying power profile
@@ -120,8 +141,9 @@ void MCPMPCoeffs::Run() {
   gsl_matrix_complex_set_all(ch,z);
   for (int i=0; i<_M; i++) { // user i (tx)
     for (int ii=0;ii<_M;ii++) { // user ii (rx)
+      double plgain = gain * gsl_matrix_get(pathLoss,i,ii);
       for (int j=0; j<L(); j++) { // tap j 
-	double coeffstd=gain*exp(-j/PTau())/sqrt(2.0);
+	double coeffstd=plgain*exp(-j/PTau())/sqrt(2.0);
 	if (j==0) {  // if this is the first tab 
 	  chcoeff = gsl_complex_rect( gsl_ran_gaussian(ran,coeffstd)+gainrice,
 	   			      gsl_ran_gaussian(ran,coeffstd));
@@ -151,10 +173,12 @@ void MCPMPCoeffs::Run() {
 
 void MCPMPCoeffs::Finish() {
   gsl_matrix_complex_free(ch);
+  gsl_matrix_free(pathLoss);
   gsl_rng_free( ran );
   if (geoEnabled) {
     ofs.close();
     gsl_vector_complex_free(geoPositions);
+    gsl_vector_complex_free(geoVelocities);
   }
 }
 
@@ -162,57 +186,115 @@ void MCPMPCoeffs::GeoInit() {
 
   // initial geo positions
 
-  geoPositions = gsl_vector_complex_alloc(M());
-  for (int i=0;i<M();i++) {
+  geoPositions = gsl_vector_complex_alloc(2*M());
+  geoVelocities  = gsl_vector_complex_alloc(2*M());
+
+  for (int i=0;i<2*M();i++) {
     double lat=gsl_ran_flat(ran,
 			     GEO_AREA_CENTER_LAT-GEO_AREA_SIZE/2.0,
 			     GEO_AREA_CENTER_LAT+GEO_AREA_SIZE/2.0);
     double lon=gsl_ran_flat(ran,
 			     GEO_AREA_CENTER_LON-GEO_AREA_SIZE/2.0,
 			     GEO_AREA_CENTER_LON+GEO_AREA_SIZE/2.0);
+    double velmod=gsl_ran_flat(ran,
+			     GEO_VELOCITY_MIN,
+			     GEO_VELOCITY_MAX);
+    double veldir=gsl_ran_flat(ran,
+			       0.0,
+			       2*M_PI);
     gsl_vector_complex_set(geoPositions,i,gsl_complex_rect(lat,lon));
+    gsl_vector_complex_set(geoVelocities,i,gsl_complex_polar(velmod,veldir));
 
   }
 
   // header and footer of kml section
   kmlhead << "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" << endl
-	  << "<kml xmlns=\"http://earth.google.com/kml/2.0\">" << endl;
+	  << "<kml xmlns=\"http://earth.google.com/kml/2.0\">" << endl
+	  << "<Document>" << endl
+	  << "<name>MuDiSP User locations</name>" << endl
+	  << "<Style id=\"greenIcon\">" << endl
+	  << "\t<IconStyle>" << endl
+	  << "\t\t<scale>2.0</scale>" << endl
+	  << "\t\t<Icon>" << endl 
+	  << "\t\t\t<href>http://maps.google.com/mapfiles/ms/icons/yellow-dot.png</href>" << endl
+	  << "\t\t</Icon>" << endl
+	  << "\t</IconStyle>" << endl
+	  << "</Style>" << endl
+	  << "<Style id=\"blueIcon\">" << endl
+	  << "\t<IconStyle>" << endl
+	  << "\t\t<scale>2.0</scale>" << endl
+	  << "\t\t<Icon>" << endl 
+	  << "\t\t\t<href>http://maps.google.com/mapfiles/ms/icons/red-dot.png</href>" << endl
+	  << "\t\t</Icon>" << endl
+	  << "\t</IconStyle>" << endl
+	  << "</Style>" << endl;
 
-  kmlheadend << "</kml>" << endl;
-  
+
+  kmlheadend << "</Document>" << endl
+	     << "</kml>" << endl;
+
 }
 
 
 
 void MCPMPCoeffs::GeoUpdate(double seconds) {
+
+  for (int i=0;i<2*M();i++) {
+
+    gsl_complex v = gsl_vector_complex_get(geoVelocities,i);
+    gsl_complex p = gsl_vector_complex_get(geoPositions,i);
+    gsl_complex np = gsl_complex_add(p,gsl_complex_mul_real(v, seconds));
+
+    gsl_vector_complex_set(geoPositions,i,np);
+
+  }
+
+  
+
 }
 
 
 
 void MCPMPCoeffs::GeoRender() {
 
-  kmlobject.seekp(ios_base::beg);
+  ostringstream kmltmp;
 
-  for (int i=0;i<1;i++) { 
+  //  kmlobject.seekp(ios_base::beg);
+
+  for (int i=0;i<M();i++) { // tx loop
 
     gsl_complex pos = gsl_vector_complex_get(geoPositions,i);
 
-    kmlobject.precision(10);
-    kmlobject << "\t<Placemark>" << endl
-	      << "\t\t<name>User " << i << "</name>" << endl
+    kmltmp.precision(10);
+    kmltmp << "\t<Placemark>" << endl
+	      << "\t\t<name>Tx_" << i << "</name>" << endl
 	      << "\t\t<description>^</description>" << endl
-	      << "\t<LookAt>" << endl
-	      << "\t\t<longitude>" << GEO_AREA_CENTER_LON << "</longitude>" << endl
-	      << "\t\t<latitude>"<< GEO_AREA_CENTER_LAT << "</latitude>" << endl
-	      << "\t\t<range>" << 1000 << "</range>" << endl
-	      << "\t\t<tilt>" << 0 << "</tilt>" << endl
-	      << "\t\t<heading>" << 0 << "</heading>" << endl
-	      << "\t</LookAt>" << endl
+	      << "\t\t<styleUrl>#greenIcon</styleUrl>" << endl      
 	      << "\t\t<Point>" << endl
-	      << "\t\t\t<coordinates>" << GSL_IMAG(pos) << "," << GSL_REAL(pos) << "," << 0 << "</coordinates>" << endl
+	      << "\t\t\t<coordinates>" << GSL_IMAG(pos) << "," 
+	      << GSL_REAL(pos) << "," 
+	      << 0 << "</coordinates>" << endl
 	      << "\t\t</Point>" << endl
 	      << "\t</Placemark>" << endl;
   }
+
+  for (int i=0;i<M();i++) { // rx loop
+    
+    gsl_complex pos = gsl_vector_complex_get(geoPositions,i+_M);
+    
+    kmltmp.precision(10);
+    kmltmp << "\t<Placemark>" << endl
+	      << "\t\t<name>Rx_" << i << "</name>" << endl
+	      << "\t\t<description>^</description>" << endl
+	      << "\t\t<styleUrl>#blueIcon</styleUrl>" << endl      
+	      << "\t\t<Point>" << endl
+	      << "\t\t\t<coordinates>" << GSL_IMAG(pos) << "," 
+	      << GSL_REAL(pos) << "," 
+	      << 0 << "</coordinates>" << endl
+	      << "\t\t</Point>" << endl
+	      << "\t</Placemark>" << endl;
+  }
+
 
   ofs.open(GeoFn());  
   if (! ofs ) {
@@ -221,8 +303,52 @@ void MCPMPCoeffs::GeoRender() {
     exit(_ERROR_OPEN_FILE_);
   }
 
-  ofs << kmlhead.str() << kmlobject.str() << kmlheadend.str();
+  ofs << kmlhead.str() << kmltmp.str() << kmlheadend.str();
   ofs.close();
 
 }
 
+
+
+double MCPMPCoeffs::GeoDistance(unsigned int tx, unsigned int rx) {
+  gsl_complex txPos =  gsl_vector_complex_get(geoPositions,tx); // lat,lon
+  gsl_complex rxPos =  gsl_vector_complex_get(geoPositions,rx+_M); // lat,lon
+
+  double txPosLat = GSL_REAL(txPos)*M_PI_OVER_180;
+  double txPosLon = GSL_IMAG(txPos)*M_PI_OVER_180;
+  double rxPosLat = GSL_REAL(rxPos)*M_PI_OVER_180;
+  double rxPosLon = GSL_IMAG(rxPos)*M_PI_OVER_180;
+
+  double deltaLat = rxPosLat-txPosLat; // ok 
+  double deltaLon = rxPosLon-txPosLon; // be careful around 180E/W
+  double meanLat = (rxPosLat+txPosLat)/2.0; // be careful around poles
+
+  if (deltaLon > M_PI) // es Lon1 =-179 Lon2=179 D=2
+    deltaLon -= 2*M_PI;
+  if (deltaLon < -M_PI)
+    deltaLon += 2*M_PI;
+
+  double tmparg = gsl_sf_cos(meanLat)*deltaLon;
+
+  return EARTH_RADIUS * gsl_hypot( deltaLat,tmparg );
+
+}
+
+
+void MCPMPCoeffs::PathLossUpdate() {
+
+  for (int i=0; i<_M; i++) { // user i (tx)
+    for (int ii=0;ii<=i;ii++) { // user ii (rx)
+      
+      double ploss = gsl_pow_3(ZeroDb()/GeoDistance(i,ii));
+      // cout << "GeoDistance=" << GeoDistance(i,ii) 
+      // 	   << "\tploss=" << ploss 
+      // 	   << "\ti=" << i 
+      // 	   << "\tii=" << ii << endl;
+ 
+     gsl_matrix_set(pathLoss,i,ii,ploss);
+     gsl_matrix_set(pathLoss,ii,i,ploss); // it's symmetric !	
+      
+    }
+  }
+}
--- mudisp-4-examples/gmc-cdma/src/mcpmpcoeffs.h
+++ mudisp-4-examples/gmc-cdma/src/mcpmpcoeffs.h
@@ -16,9 +16,29 @@
 #include <fstream>
 #include <sstream>
 
-#define GEO_AREA_CENTER_LON 11.252945
-#define GEO_AREA_CENTER_LAT 43.799908
-#define GEO_AREA_SIZE 0.000010
+#define GEO_AREA_CENTER_LAT 29.964324
+#define GEO_AREA_CENTER_LON 30.893594
+#define GEO_AREA_SIZE 0.01 // in dec degrees
+#define GEO_VELOCITY_MIN 0.0 
+#define GEO_VELOCITY_MAX 0.00000436 // 100 kmph (rad/s = km/h / 3.6 / 1.11 * 0.00001 / 360 * 2 PI )
+#define GEO_UPDATE_INTERVAL 1000
+#define EARTH_RADIUS 6371.009 // km
+#define M_PI_OVER_180 M_PI/180.0
+
+
+// Accuracy versus decimal places at the equator
+// decimal
+// places	degrees		distance
+// 0		1.0    		111 km
+// 1		0.1		11.1 km
+// 2		0.01		1.11 km
+// 3		0.001		111 m
+// 4		0.0001		11.1 m
+// 5		0.00001		1.11 m
+// 6		0.000001	0.111 m
+// 7		0.0000001	1.11 cm
+// 8		0.00000001	1.11 mm
+//
 
 ////////
 ////////   Here put your personal includes
@@ -33,18 +53,21 @@ class MCPMPCoeffs : public Block {
   IntParam N,L,M;
   FloatParam PTau;
   FloatParam Ricean;
+  FloatParam ZeroDb;
   StringParam GeoFn;
 
   ////////   Local Attributes
 
   double gain, gainrice;
   gsl_matrix_complex *ch;
+  gsl_matrix *pathLoss;
   gsl_rng * ran; 
-  unsigned int _M;
+  unsigned int _M, runCount;
+
 
   // Geo Model
 
-  gsl_vector_complex *geoPositions; // node coordinates
+  gsl_vector_complex *geoPositions, *geoVelocities; // node coordinates tx + rx (2xM)
   unsigned int geoEnabled;
   ostringstream kmlhead,kmlobject,kmlheadend;
   ofstream ofs;
@@ -66,6 +89,7 @@ class MCPMPCoeffs : public Block {
     ,PTau("PowerTau",1.0,"decay factor of delay spread")
     ,Ricean("RiceFact",0.0,"Ricean factor of channel")
     ,GeoFn("GeoFName","none","kml for georendering (none to disable)")
+    ,ZeroDb("ZeroDb",0.01,"zero dB distance for path loss (km)")
     {
 
       //////// local parameter registration
@@ -75,7 +99,7 @@ class MCPMPCoeffs : public Block {
       AddParameter(PTau);
       AddParameter(Ricean);
       AddParameter(GeoFn);
-
+      AddParameter(ZeroDb);
     }
 
   ~MCPMPCoeffs() {
@@ -90,7 +114,8 @@ class MCPMPCoeffs : public Block {
   void GeoInit();
   void GeoUpdate(double seconds=1.0);
   void GeoRender();
-
+  double GeoDistance(unsigned int,unsigned int);
+  void PathLossUpdate();
   
 };
 

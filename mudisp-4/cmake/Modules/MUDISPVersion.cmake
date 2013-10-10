07757b1eeb176c9e098335d0873d23af38713783
innocentimassimo <innocentimassimo.maxx@gmail.com>
2013-10-10 23:29:39 +0200

    Story 5: Minor Release number update

--- mudisp-4/cmake/Modules/MUDISPVersion.cmake
+++ mudisp-4/cmake/Modules/MUDISPVersion.cmake
@@ -5,5 +5,5 @@ INCLUDE(MUDISPPython) #requires python for parsing
 # Setup Version Numbers
 ########################################################################
 SET(MUDISP_VERSION_MAJOR 004)  #API compatibility number
-SET(MUDISP_VERSION_MINOR 0)    #Timestamp of git commit
+SET(MUDISP_VERSION_MINOR 1)    #Timestamp of git commit
 SET(MUDISP_VERSION_PATCH 0)    #Short hash of git commit

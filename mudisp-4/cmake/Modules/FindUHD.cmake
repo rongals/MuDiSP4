28aeb5cde46586bb9f9edf7fd727d48125e039f2
innocentimassimo <innocentimassimo.maxx@gmail.com>
2013-10-11 06:23:52 +0200

    Fix cmake find Module

--- mudisp-4/cmake/Modules/FindUHD.cmake
+++ mudisp-4/cmake/Modules/FindUHD.cmake
@@ -1,18 +1,27 @@
 ########################################################################
 # Find the library for the USRP Hardware Driver
 ########################################################################
-
+if(NOT UHD_FOUND)
 INCLUDE(FindPkgConfig)
 PKG_CHECK_MODULES(PC_UHD uhd)
 
 FIND_PATH(
-    UHD_INCLUDE_DIRS
+    UHD_INCLUDE_DIR
     NAMES uhd/config.hpp
     HINTS $ENV{UHD_DIR}/include
         ${PC_UHD_INCLUDEDIR}
     PATHS /usr/local/include
           /usr/include
 )
+
+#find_path(UHD_INCLUDE_DIR NAMES libusb.h
+#    PATHS
+#    ${LIBUSB_PKG_INCLUDE_DIRS}
+#    /usr/include/libusb-1.0
+#    /usr/include
+#    /usr/local/include
+#  )
+
 
 FIND_LIBRARY(
     UHD_LIBRARIES
@@ -22,7 +31,20 @@ FIND_LIBRARY(
     PATHS /usr/local/lib
           /usr/lib
 )
+message(STATUS "UHD include dir: ${UHD_INCLUDE_DIR}")
+message(STATUS "UHD include dir: ${UHD_INCLUDE_DIR}")
+#INCLUDE(FindPackageHandleStandardArgs)
+#FIND_PACKAGE_HANDLE_STANDARD_ARGS(UHD DEFAULT_MSG UHD_LIBRARIES UHD_INCLUDE_DIRS)
+if(UHD_INCLUDE_DIR AND UHD_LIBRARIES)
+  set(UHD_FOUND TRUE CACHE INTERNAL "libuhd found")
+  message(STATUS "Found libuhd: ${UHD_INCLUDE_DIR}, ${UHD_LIBRARIES}")
+else(UHD_INCLUDE_DIR AND UHD_LIBRARIES)
+  set(UHD_FOUND FALSE CACHE INTERNAL "libuhd found")
+  message(STATUS "libuhd not found.")
+endif(UHD_INCLUDE_DIR AND UHD_LIBRARIES)
 
-INCLUDE(FindPackageHandleStandardArgs)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(UHD DEFAULT_MSG UHD_LIBRARIES UHD_INCLUDE_DIRS)
 MARK_AS_ADVANCED(UHD_LIBRARIES UHD_INCLUDE_DIRS)
+endif(NOT UHD_FOUND)
+
+
+
